e52521c85c8c8ee0e5bbefd18d1504f7aa6914b0
tuancq <tuancq2011@gmail.com>
2022-05-11 22:41:41 +0700

    adding CSharp script t5

---
+++ CSharp-t5/CSharp-t5/Collections/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Input number from 0 - 4 for the lucky present: ");
+int number = int.Parse(Console.ReadLine());
+
+string[] present = { "Candy", "Teddy bear", "Shirt", "100$ dolars", "Water bottle" };
+
+Console.WriteLine($"Congratulation! You have won the present of {present[number]}");
+
+
---
+++ CSharp-t5/CSharp-t5/ConditionAndLoops/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hi, what would you like to drink?");
+Console.WriteLine("Input option: 1/. Coffee      2/. Tea");
+int option = int.Parse(Console.ReadLine());
+
+switch (option)
+{
+    case 1:
+        Console.WriteLine("Here is your coffe, have a great day!");
+        break;
+    case 2:
+        Console.WriteLine("Here is your tea, enjoy!");
+        break;
+}
+
+Console.ReadKey();
+
