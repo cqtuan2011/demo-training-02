ef8dfc62ba204a33698b7f811a6034183df903c9
tuancq <tuancq2011@gmail.com>
2022-05-13 11:01:55 +0700

    adding CSharp t6

---
+++ CSharp-t6/CSharp-t6/CSharp-t6/Program.cs
@@ -0,0 +1,16 @@
+﻿IDictionary<int, string> numberNames = new Dictionary<int, string>();
+numberNames.Add(1, "One"); 
+numberNames.Add(2, "Two");
+numberNames.Add(3, "Three");
+
+foreach (KeyValuePair<int, string> kvp in numberNames)
+	Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+
+var cities = new Dictionary<string, string>(){
+	{"UK", "London, Manchester, Birmingham"},
+	{"USA", "Chicago, New York, Washington"},
+	{"India", "Mumbai, New Delhi, Pune"}
+};
+
+foreach (var kvp in cities)
+	Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
